3e8415c73c520e86c673f2c98f8af8da29145fef
ShaggyGettu <jhfsa1@gmail.com>
2021-07-10 17:16:54 +0300

    circle numbers completed

--- lib/AnalogClock.dart
+++ lib/AnalogClock.dart
@@ -1,15 +1,10 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as intl;
 
 class AnalogClock extends StatefulWidget {
-  final double radius;
+  final double radius = 180;
 
-  const AnalogClock({
-    Key key,
-    this.radius,
-  }) : super(key: key);
+  const AnalogClock({Key key}) : super(key: key);
   @override
   _AnalogClockState createState() => _AnalogClockState();
 
@@ -22,7 +17,6 @@ class AnalogClock extends StatefulWidget {
       context: context,
       builder: (context1) => StatefulBuilder(
         builder: (context2, setState) {
-          print(MediaQuery.of(context2).size);
           return AlertDialog(
             contentPadding: EdgeInsets.all(0),
             shape: RoundedRectangleBorder(
@@ -66,11 +60,55 @@ class AnalogClock extends StatefulWidget {
                 ],
               ),
             ),
-            backgroundColor: Colors.white,
             elevation: 0,
             content: Container(
-              child: AnalogClock(
-                radius: MediaQuery.of(context2).size.width,
+              height: 408,
+              padding: EdgeInsets.zero,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnalogClock(),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Row(
+                      textDirection: TextDirection.rtl,
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        FlatButton(
+                          minWidth: 24,
+                          highlightColor: Colors.white,
+                          splashColor: Colors.white,
+                          padding: EdgeInsets.zero,
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text(
+                            "OK",
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                        ),
+                        FlatButton(
+                          highlightColor: Colors.white,
+                          splashColor: Colors.white,
+                          minWidth: 24,
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text(
+                            "CANCEL",
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           );
@@ -85,11 +123,21 @@ class _AnalogClockState extends State<AnalogClock> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(
+        top: 77,
+        left: 37,
+        right: 37,
+        bottom: 77,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      width: widget.radius * 2,
+      height: widget.radius * 2,
       child: Stack(
         children: [
-          ClockNumbers(
-            radius: widget.radius,
-          ),
           CustomPaint(
             size: Size(
               widget.radius,
@@ -98,6 +146,9 @@ class _AnalogClockState extends State<AnalogClock> {
             painter: ClockPainter(
               size: Size(widget.radius, widget.radius),
             ),
+          ),
+          ClockNumbers(
+            radius: widget.radius,
           ),
         ],
       ),
@@ -115,6 +166,7 @@ enum ClockType {
 class ClockNumbers extends StatefulWidget {
   /// The size of the rounded numbers, `width` diameter.
   final double radius;
+
   final ClockType clockType;
 
   ClockNumbers({
@@ -142,72 +194,79 @@ class _ClockNumbersState extends State<ClockNumbers> {
         // TODO: Handle this case.
         break;
     }
-    return Container(
-      child: Stack(
-        children: numbers,
-      ),
+    return Stack(
+      children: numbers,
     );
   }
 
   List<Widget> build12HoursNumbers() {
     List<Widget> numbers = [];
-
-    numbers.add(
-      Positioned(
-        top: 0,
-        left: widget.radius / 4,
-        child: FlatButton(
-          onPressed: null,
-          child: Text(
-            '12',
-            style: TextStyle(fontSize: 16),
-          ),
-        ),
-      ),
-    );
-    for (int i = 1; i <= 2; i++) {
+    double width = widget.radius / 2;
+    double height = 0;
+    double spacer = 18;
+    for (int i = 0; i < 3; i++) {
+      // 0-3
       numbers.add(
         Positioned(
-          top: widget.radius / 2 * (i / 3),
-          left: widget.radius / 2 * ((12 - i - 1) / 12),
-          child: FlatButton(
-            child: Text(
-              i.toString(),
-              style: TextStyle(fontSize: 16),
-            ),
-            onPressed: null,
-          ),
+          height: 26,
+          width: 26,
+          top: height + (i / 6) * widget.radius - (i == 0 ? 0 : spacer),
+          left: width + (i / 6) * widget.radius + (i == 0 ? 0 : spacer),
+          child: numberButton(i == 0 ? 12 : i),
         ),
       );
-      // int realNumber = i + pow(2, (3 - i));
-      // numbers.add(
-      //   Positioned(
-      //     top: widget.size.height * (realNumber / 12),
-      //     left: widget.size.width * (i / 6),
-      //     child: FlatButton(
-      //       child: Text(
-      //         (realNumber).toString(),
-      //         style: TextStyle(fontSize: 16),
-      //       ),
-      //       onPressed: null,
-      //     ),
-      //   ),
-      // );
+      // 3-6
+      numbers.add(
+        Positioned(
+          height: 26,
+          width: 26,
+          top: width + (i / 6) * widget.radius + (i == 0 ? 0 : spacer),
+          left: width + ((3 - i) / 6) * widget.radius + (i == 0 ? 0 : spacer),
+          child: numberButton(i + 3),
+        ),
+      );
+      // 6-9
+      numbers.add(
+        Positioned(
+          height: 26,
+          width: 26,
+          top: width + ((3 - i) / 6) * widget.radius + (i == 0 ? 0 : spacer),
+          left: width - (i / 6) * widget.radius - (i == 0 ? 0 : spacer),
+          child: numberButton(i + 6),
+        ),
+      );
+      // 9-12
+      numbers.add(
+        Positioned(
+          height: 26,
+          width: 26,
+          top: width - (i / 6) * widget.radius - (i == 0 ? 0 : spacer),
+          left: height + (i / 6) * widget.radius - (i == 0 ? 0 : spacer),
+          child: numberButton(i + 9),
+        ),
+      );
     }
-    // numbers.add(
-    //   Positioned(
-    //     top: widget.size.height / 2,
-    //     left: widget.size.width / 2,
-    //     child: FlatButton(
-    //       onPressed: null,
-    //       child: Text(
-    //         '3',
-    //         style: TextStyle(fontSize: 16),
-    //       ),
-    //     ),
-    //   ),
-    // );
+
     return numbers;
+  }
+
+  RawMaterialButton numberButton(int number) {
+    return RawMaterialButton(
+      onPressed: () => print(number),
+      hoverElevation: 8,
+      child: numberText(number),
+      shape: CircleBorder(),
+    );
+  }
+
+  Text numberText(int number) {
+    return Text(
+      (number).toString(),
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+      ),
+    );
   }
 }
 
